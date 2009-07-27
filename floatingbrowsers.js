5b095af7f8613d27547d7a320c035f71a5069665
Rohit Pidaparthi <rohitpid@gmail.com>
2009-07-27 10:58:20 -0700

    Removed all canvas3d unused files

--- floatingbrowsers.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){  
-  $("#dialog").dialog({ autoOpen: false });
-  $("a#dialog_link").click(function(){$("#dialog").dialog("open");});
-  $("#dialog2").dialog({ autoOpen: false });
-  $("a#dialog_link2").click(function(){$("#dialog2").dialog("open");});
-});
