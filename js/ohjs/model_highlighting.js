efe3142607677b199bd025f84c277b0de6e2ef65
Rohit Pidaparthi <rohitpid@gmail.com>
2009-10-04 23:02:27 -0700

    Updated copyright info for all js files

--- js/ohjs/model_highlighting.js
+++ js/ohjs/model_highlighting.js
@@ -1,4 +1,20 @@
 /*
+ * Copyright 2009 Rohit Pidaparthi
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Author: Rohit Pidaparthi <rohitpid@gmail.com>
+ *
+ */
+
+
+/*
  * Function that highlights the the object by increasing or decreasing its brightness
  * i.e the brightness, by converting RGB to HSV and back
  * Takes a shape argument which is the model to be highlighted
